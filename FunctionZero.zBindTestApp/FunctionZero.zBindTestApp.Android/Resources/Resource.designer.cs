f1bdd8ebd054b3206c8bb581b493f5d7553ccb08
Keith Pickford <Keith@FunctionZero.com>
2023-01-15 14:45:28 +0000

    NuGet bumps.

--- FunctionZero.zBindTestApp/FunctionZero.zBindTestApp.Android/Resources/Resource.designer.cs
+++ FunctionZero.zBindTestApp/FunctionZero.zBindTestApp.Android/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace FunctionZero.zBindTestApp.Droid
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.3.99.16")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.1.99.65")]
 	public partial class Resource
 	{
 		
